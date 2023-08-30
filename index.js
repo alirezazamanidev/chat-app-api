7c948f9f0edd7ebb0c47c56ee4c55c7f77139693
Alireza zamani <zamani802000@gmail.com>
2023-08-31 01:10:54 +0330

    fix commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https:chat.webidemyyy.ir",
+    origin: "https://chat.webidemyyy.ir",
     credentials: true,
   },
 });
